79f0485271fc90f5e312a7f9b6a72284846de3cf
muzammil safdar <mmuzammil327off@gmail.com>
2024-10-29 10:43:27 +0500

    updated protected page

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,24 +1,31 @@
-'use client'
-import { useSession, signIn } from 'next-auth/react';
+// 'use client'
+// import { useSession, signIn } from 'next-auth/react';
 
-const ProtectedPage = () => {
-    const { data: session } = useSession();
+// const ProtectedPage = () => {
+//     const { data: session } = useSession();
 
-    if (!session) {
-        return (
-            <div>
-                <h1>You must be logged in to view this page</h1>
-                <button onClick={() => signIn()}>Sign in</button>
-            </div>
-        );
-    }
+//     if (!session) {
+//         return (
+//             <div>
+//                 <h1>You must be logged in to view this page</h1>
+//                 <button onClick={() => signIn()}>Sign in</button>
+//             </div>
+//         );
+//     }
 
+//     return (
+//         <div>
+//             <h1>Protected Page</h1>
+//             <p>Welcome, {session.user?.name}</p>
+//         </div>
+//     );
+// };
+
+// export default ProtectedPage;
+import React from 'react'
+
+export default function page() {
     return (
-        <div>
-            <h1>Protected Page</h1>
-            <p>Welcome, {session.user?.name}</p>
-        </div>
-    );
-};
-
-export default ProtectedPage;
+        <div>page</div>
+    )
+}
